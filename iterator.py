0949efae5ee6740e749e56bfdbdfe78074b39c28
wantoone <wantoone777@gmail.com>
2023-02-12 15:01:59 +0300

    "resolved iterators and gens"

--- iterator.py
+++ iterator.py
@@ -1,6 +1,5 @@
 
 
-
 class Yes(object):
 
     def __init__(self, stop):
@@ -13,11 +12,10 @@ class Yes(object):
     def __next__(self):
         if self.x < self.stop:
             self.x += 1
-            return 'yes'
+            return 'yes {0}'.format(self.x - 1)
         else:
             # Iterators must raise when done, else considered broken
             raise StopIteration
-
 
 
 # my_iter1 = Yes(3)
@@ -39,27 +37,104 @@ class Yes(object):
 
 
 
-l = ['12', 'fdsa', 505, 'alex', 'Moscow']
+# l = [1, 2, 3, 4, 5, 6, 7]
+
+
+# for i in l:
+#     temp = i * i
+#     print(temp)
+
+# print('#' * 8)
+
+# iter_obj = l.__iter__()
+# while True:
+#     try:
+#         i = iter_obj.__next__()
+
+#         # Code block
+#         temp = i * i
+#         print(temp)
+#         # End Code block
+        
+        
+#     except StopIteration:
+#         break
 
 
 
-for i in l:
+# rang = range(15)
+
+# for i in rang:
+#     print(i)
+
+
+# # second way via iter and next funcs
+# iter_obj = iter(rang)
+# while True:
+#     try:
+#         i = next(iter_obj)
+        
+#         print(i)
+    
+#     except StopIteration:
+#         break
+
+
+def gen(n):
+    for i in range(n):
+        yield i
+
+
+def gen2(n):
+    iter_obj = range(n).__iter__()
+
+    while True:
+        try:
+            i = iter_obj.__next__()
+            
+            yield i
+            
+        except StopIteration:
+            break
+
+
+g = gen(5)
+# 1
+# r = g.__next__()
+# print(r)
+# r = g.__next__()
+# print(r)
+# r = g.__next__()
+# print(r)
+# r = g.__next__()
+# print(r)
+# r = g.__next__()
+# print(r)
+# try:
+#     r = g.__next__()
+#     print(r)
+# except:
+#     print('Done')
+
+
+# 2
+# iter_obj = g.__iter__() # generator object technically
+# while True:
+#     try:
+#         i = iter_obj.__next__()
+        
+#         print(i)
+        
+#     except StopIteration:
+#         break
+
+
+# 3
+for i in g:
     print(i)
 
 
 
-iter_obj = l.__iter__()
-while True:
-    try:
-        i = iter_obj.__next__()
 
-        # Code block
-        print(i)
-        # End Code block
-        
-        
-    except StopIteration:
-        break
-
-
-
+# for i in [1, 2, 3]:
+#     print(i)
